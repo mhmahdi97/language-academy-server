6f761985ba7799c65609b4ddda022196d5208f40
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-12 19:39:33 +0600

    created jwt verify middleware

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 
 
@@ -42,7 +59,6 @@ async function run() {
       res.send({ token })
     })
 
-    console.log(process.env.ACCESS_TOKEN_SECRET)
 
     // users related apis
     app.get('/all-users', async (req, res) => {
