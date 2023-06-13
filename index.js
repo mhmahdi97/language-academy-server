4dfe069f1b1b0097e9af6400dea49504471a8c2b
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 04:03:30 +0600

    created api to get specific course with id

--- index.js
+++ index.js
@@ -108,6 +108,13 @@ async function run() {
       res.send(result)
     });
 
+    app.get('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await courseCollection.findOne(query);
+      res.send(result);
+    })
+
     app.get('/instructor-courses', async (req, res) => {
       let query = {};
 
