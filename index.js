0db2fa5149f1adedd6979f8496b75764b216e672
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 14:20:48 +0600

    createde verify instructor

--- index.js
+++ index.js
@@ -69,6 +69,17 @@ async function run() {
       }
       next();
     }
+    
+    // verify admin
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'instructor') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
 
     // users related apis
