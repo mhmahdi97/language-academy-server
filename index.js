81dbea1b83fae8b0db0bc44dc1ae9df619f700f7
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 11:40:57 +0600

    created api to get admin

--- index.js
+++ index.js
@@ -76,6 +76,19 @@ async function run() {
       res.send(result)
     });
 
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
 
     app.post('/users', async (req, res) => {
       const user = req.body;
