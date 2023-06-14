8a7f05b0224f8a6484cfeaf9185d566e91a42f44
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 14:19:26 +0600

    created verify admin middleware

--- index.js
+++ index.js
@@ -59,6 +59,17 @@ async function run() {
       res.send({ token })
     })
 
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
 
     // users related apis
     app.get('/all-users', async (req, res) => {
