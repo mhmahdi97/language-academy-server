6aaf271bc62f2099b5e4003000eda4af2e636006
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 07:49:35 +0600

    created api to make admin and make instructor

--- index.js
+++ index.js
@@ -90,6 +90,36 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
 
     // courses related apis
     app.get('/all-courses', async (req, res) => {
